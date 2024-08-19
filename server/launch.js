526eb98df3ce04be4181e6cd1d5be95e2e8fb4c8
Joel Ayoub <jnayoub@gmail.com>
2024-08-18 23:35:31 -0400

    update launch.js file to pull env PORT if provided (usually during real deploy boot sequence) otherwise use the standard localhost port of 3000.

--- server/launch.js
+++ server/launch.js
@@ -47,7 +47,7 @@ app.post("/gemini-prompt", async (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
